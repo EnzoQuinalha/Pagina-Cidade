42461733601b5e4305fa9497e015a390be22d521
Enzo Quinlha <159260763+EnzoQuinalha@users.noreply.github.com>
2024-07-31 09:39:07 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let cid = document.getElementById('img1')
+let leg = document.getElementById('legenda')
+function um(){
+    cid.src="veneza1.jfif"
+    leg.innerHTML="Canal localizado em Veneza, que demonstra um breve exemplo de vários outros."
+}
+function dois(){
+    cid.src="veneza2.jfif"
+    leg.innerHTML="Ponte de Rialto."
+}
+function tres(){
+    cid.src="veneza3.jfif"
+    leg.innerHTML="Um dos diversos pontos turisticos em Veneza, um restaurante entre muitos ontem pode-se chegar de barco ou andando."
+}
+function quatro(){
+    cid.src="veneza4.jfif"
+    leg.innerHTML="Uma foto panoramica de Veneza."
+}
